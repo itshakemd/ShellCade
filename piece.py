2bd090ab06518ffe221b99fa8098d1095e9d6836
itshakemd <itshakemd@gmail.com>
2026-06-15 13:00:53 +0300

    feat:compute absolute board cells for piece

--- piece.py
+++ piece.py
@@ -9,3 +9,11 @@ class Piece:
         self.rot = 0
         self.x = 3
         self.y = 0
+
+    def cells(self, rot=None, x=None, y=None):
+        rot = self.rot if rot is None else rot
+        x = self.x if x is None else x
+        y = self.y if y is None else y
+        states = SHAPES[self.kind]
+        shape = states[rot % len(states)]
+        return [(x + cx, y + cy) for cx, cy in shape]
