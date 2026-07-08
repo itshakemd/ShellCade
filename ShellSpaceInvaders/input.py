a0528ffaf4b43b23f7f23a8c644fce609200b81a
itshakemd <itshakemd@gmail.com>
2026-07-08 19:54:55 +0300

    feat: add Space Invaders keyboard reader

---
+++ ShellSpaceInvaders/input.py
@@ -0,0 +1,13 @@
+"""Non-blocking Windows keyboard input."""
+
+import msvcrt
+
+
+def read_keys():
+    keys = set()
+    while msvcrt.kbhit():
+        key = msvcrt.getch()
+        if key in (b"\x00", b"\xe0"):
+            key = msvcrt.getch()
+        keys.add(key)
+    return keys
