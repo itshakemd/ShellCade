300f69a080b7baf772588b935ca6dab3ef3745ed
itshakemd <itshakemd@gmail.com>
2026-07-10 22:05:50 +0300

    test: cover cannon bounds firing and hits

---
+++ ShellSpaceInvaders/test_game.py
@@ -0,0 +1,31 @@
+import unittest
+
+from constants import PLAYER_Y
+from game import InvadersGame
+
+
+class InvadersTests(unittest.TestCase):
+    def test_player_stays_inside_bounds(self):
+        game = InvadersGame()
+        for _ in range(100):
+            game.move_player(-2)
+        self.assertEqual(game.player_x, 2)
+
+    def test_firing_creates_one_player_shot(self):
+        game = InvadersGame()
+        game.fire()
+        game.fire()
+        self.assertEqual(len(game.projectiles), 1)
+        self.assertEqual(game.projectiles[0].y, PLAYER_Y - 1)
+
+    def test_hit_removes_invader_and_scores(self):
+        game = InvadersGame()
+        enemy = game.alive_enemies[0]
+        game.projectiles = [type("Shot", (), {"x": enemy.x, "y": enemy.y, "owner": "player"})()]
+        game.resolve_hits()
+        self.assertFalse(enemy.alive)
+        self.assertGreater(game.score, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
