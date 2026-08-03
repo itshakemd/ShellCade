4f372c98d7f63d4c35b7970fd869b98316490694
itshakemd <itshakemd@gmail.com>
2026-08-03 21:50:48 +0300

    test: cover Arkanoid paddle rebounds

--- ShellArkanoid/test_game.py
+++ ShellArkanoid/test_game.py
@@ -16,6 +16,24 @@ class ArkanoidGameTests(unittest.TestCase):
         game.move_paddle(1000)
         self.assertEqual(game.paddle.right, 75)
 
+    def test_ball_bounces_from_paddle(self):
+        game = ArkanoidGame()
+        game.ball.x = game.paddle.x + 4
+        game.ball.y = 20
+        game.ball.dy = 1
+        game.tick()
+        self.assertEqual(game.ball.dy, -1)
+
+    def test_brick_collision_adds_score(self):
+        game = ArkanoidGame()
+        brick = game.bricks[0]
+        game.ball.x = brick.x
+        game.ball.y = brick.y - 1
+        game.ball.dy = 1
+        game.tick()
+        self.assertFalse(brick.alive)
+        self.assertEqual(game.score, 50)
+
 
 if __name__ == "__main__":
     unittest.main()
