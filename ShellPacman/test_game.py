7ddc5f421b7ff653510520b3346712ebab39b7bf
itshakemd <itshakemd@gmail.com>
2026-08-11 21:09:37 +0300

    test: cover Pac-Man startup and walls

---
+++ ShellPacman/test_game.py
@@ -0,0 +1,21 @@
+import unittest
+
+from ShellPacman.game import PacmanGame
+
+
+class PacmanGameTests(unittest.TestCase):
+    def test_starts_with_pellets_and_lives(self):
+        game = PacmanGame()
+        self.assertGreater(len(game.pellets), 0)
+        self.assertEqual(game.lives, 3)
+
+    def test_walls_block_movement(self):
+        game = PacmanGame()
+        game.pacman.x, game.pacman.y = 1, 1
+        game.set_direction((-1, 0))
+        game.tick()
+        self.assertEqual((game.pacman.x, game.pacman.y), (1, 1))
+
+
+if __name__ == "__main__":
+    unittest.main()
